6f8a3e5a23f239f52caa44797799ac5d3032250a
sdvinukonda <49005831+sdvinukonda@users.noreply.github.com>
2019-04-18 16:53:26 -0700

    updated the comments

--- if_else.py
+++ if_else.py
@@ -1,3 +1,7 @@
+#If  is odd, print Weird
+#If  is even and in the inclusive range of 2 to 5 , print Not Weird
+#If  is even and in the inclusive range of 6 to 20, print Weird
+#If  is even and greater than 20, print Not Weird
 n = int(input(""))
 if n % 2 == 1:
     print("Weird")
