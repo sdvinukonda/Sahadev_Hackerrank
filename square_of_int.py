313c6ded70705f6b3025339cf268e98efdad940b
sdvinukonda <49005831+sdvinukonda@users.noreply.github.com>
2019-04-18 16:54:55 -0700

    Update square_of_int.py

--- square_of_int.py
+++ square_of_int.py
@@ -1,6 +1,12 @@
 #Read an integer N . For all non-negative integers i<n , print i^2
 n = int(input())
-#[print(i**2) for i in range(n)]
 for i in range(0,n):
     print(i*i)
     i= i+1
+#######
+#counter = 0;
+   # while  counter < n :
+       # print (counter**2)
+        #counter = counter + 1
+###########
+#[print(i**2) for i in range(n)]
